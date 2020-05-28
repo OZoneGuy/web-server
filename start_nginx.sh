37d670d4d96e8dae10b72035b3243083c256348a
OzoneGuy <alkershomar@protonmail.com>
2020-05-27 23:25:21 -0400

    fix - Fixed if statement
    Added -q flag to command

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! "`docker ps -a -f name=nginx`" ]; then
+if [ ! "`docker ps -aq -f name=nginx`" ]; then
     docker run --name nginx \
            --mount type=bind,source="$(pwd)"/content,target=/usr/share/nginx/html,readonly \
-           --mount type=bind,source="$(pwd)"/config,target=/etc/nginx/conf,readonly \
+           --mount type=bind,source="$(pwd)"/config,target=/etc/nginx,readonly \
            -p 80:80 -d \
            nginx
-elif [ "`docker inspect -f '{{.State.Running}}' nginx`" != 'true' ]; then
+elif [ "`docker inspect -f '{{.state.Running}}' nginx`" != 'true' ]; then
     docker start nginx
 fi
