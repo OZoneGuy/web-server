57712494d64125bdac8f18f3084f2fcbad332cc0
OzoneGuy <alkershomar@protonmail.com>
2020-05-27 23:46:07 -0400

    feat - Added docs directory

--- start_nginx.sh
+++ start_nginx.sh
@@ -8,6 +8,7 @@ fi
 if [ ! "`docker ps -aq -f name=nginx`" ]; then
     docker run --name nginx \
            --mount type=bind,source="$1"/content,target=/usr/share/nginx/html,readonly \
+           --mount type=bind,source="$1"/docs-html,target=/usr/share/nginx/docs,readonly \
            --mount type=bind,source="$1"/config,target=/etc/nginx,readonly \
            -p 80:80 -d \
            nginx
