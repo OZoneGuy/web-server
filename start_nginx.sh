4883c88d1c6f7dae45ecd43780532a8590fdcc85
OzoneGuy <alkershomar@protonmail.com>
2020-05-27 23:30:47 -0400

    feat - Made start_nginx more modular
    Passing root directory instead of assuming

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Please pass root directory..."
+    exit
+fi
+
 if [ ! "`docker ps -aq -f name=nginx`" ]; then
     docker run --name nginx \
-           --mount type=bind,source="$(pwd)"/content,target=/usr/share/nginx/html,readonly \
-           --mount type=bind,source="$(pwd)"/config,target=/etc/nginx,readonly \
+           --mount type=bind,source="$1"/content,target=/usr/share/nginx/html,readonly \
+           --mount type=bind,source="$1"/config,target=/etc/nginx,readonly \
            -p 80:80 -d \
            nginx
 elif [ "`docker inspect -f '{{.state.Running}}' nginx`" != 'true' ]; then
