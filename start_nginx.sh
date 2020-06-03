655e7351e1f21152c3f27706102dcbf1ef4a0bff
OzoneGuy <alkershomar@protonmail.com>
2020-06-03 16:47:34 -0400

    feat - Only require root directory when necessary

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Please pass root directory..."
-    exit
-fi
 
 if [ ! "`docker ps -aq -f name=nginx`" ]; then
+    if [ "$#" -ne 1 ]; then
+        echo "Container is not set up yet. Please pass root directory..."
+        exit
+    fi
     docker run --name nginx \
            --mount type=bind,source="$1"/content,target=/usr/share/nginx/html,readonly \
            --mount type=bind,source="$1"/docs-html,target=/usr/share/nginx/docs,readonly \
